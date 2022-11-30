ba9c6c58604465816df53c6c46fb59b5b31c0a32
Felipe Luiz <felipebluiz@gmail.com>
2022-11-30 12:37:12 -0300

    feat: favicon added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  pageExtensions: ['page.tsx', 'page.ts'],
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/_app.page.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/favicon/apple-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/favicon/apple-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/favicon/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/favicon/apple-icon-152x152.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/favicon/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/manifest.json" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
+
+export default MyApp
